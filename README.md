15c6d701321984d8fa6476084697759fe07c2050
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-05-31 17:48:46 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Phaser3 Tutorial Refactor
+
+- Based on [Phaser 3 Official Tutorial](https://phaser.io/tutorials/making-your-first-phaser-3-game)
+
+## Build
+
+```
+npm run build
+```
+
+## Local deployment
+
+You need to serve `index.html` in a server in order to run it locally.
+
+I recommend using the VScode extension [LiveServer](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer).
+
+## Visual Regression Testing
+
+After having deployed the game locally, just run:
+
+```
+npm run test
+```
---
+++ src/scripts/index.js
@@ -0,0 +1,32 @@
+import Phaser from "phaser";
+
+import { WIDTH, HEIGHT } from "./constants/scene";
+
+import preload from "./lifeCycle/preload";
+import create from "./lifeCycle/create/index";
+import update from "./lifeCycle/update";
+
+const init = () => {
+  const gravity = { y: 200 };
+  const debug = false;
+
+  new Phaser.Game({
+    type: Phaser.AUTO,
+    width: WIDTH,
+    height: HEIGHT,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity,
+        debug,
+      },
+    },
+    scene: {
+      preload,
+      create,
+      update,
+    },
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
